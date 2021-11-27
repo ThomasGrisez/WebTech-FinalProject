bee39bf4c05a45b5aa34d84739622cd18e5a01d5
ThomasGrisez <61418052+ThomasGrisez@users.noreply.github.com>
2021-11-27 19:47:34 +0100

    Update Style : Login.js & Header.js

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -1,46 +1,44 @@
-
 /** @jsxImportSource @emotion/react */
-import { useContext } from 'react';
+import { useContext } from "react";
 // Layout
-import { useTheme } from '@mui/styles';
-import { IconButton, Link } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import Context from './Context';
+import { useTheme } from "@mui/styles";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import Grid from "@mui/material/Grid";
+import Context from "./Context";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Button from "@mui/material/Button";
 
 const useStyles = (theme) => ({
   header: {
     padding: theme.spacing(1),
-    backgroundColor: 'rgba(255,255,255,.3)',
+    backgroundColor: "rgba(255,255,255,.3)",
     flexShrink: 0,
   },
   headerLogIn: {
-    backgroundColor: 'red',
+    backgroundColor: "red",
   },
   headerLogOut: {
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
   },
   menu: {
-    [theme.breakpoints.up('sm')]: {
-      display: 'none !important',
+    [theme.breakpoints.up("sm")]: {
+      display: "none !important",
     },
-  }
-})
+  },
+});
 
-export default function Header({
-  drawerToggleListener
-}) {
-  const styles = useStyles(useTheme())
-  const {
-    oauth, setOauth,
-    drawerVisible, setDrawerVisible
-  } = useContext(Context)
+export default function Header({ drawerToggleListener }) {
+  const styles = useStyles(useTheme());
+  const { oauth, setOauth, drawerVisible, setDrawerVisible } =
+    useContext(Context);
   const drawerToggle = (e) => {
-    setDrawerVisible(!drawerVisible)
-  }
+    setDrawerVisible(!drawerVisible);
+  };
   const onClickLogout = (e) => {
-    e.stopPropagation()
-    setOauth(null)
-  }
+    e.stopPropagation();
+    setOauth(null);
+  };
   return (
     <header css={styles.header}>
       <IconButton
@@ -51,17 +49,25 @@ export default function Header({
       >
         <MenuIcon />
       </IconButton>
-      Header
-      {
-        oauth ?
-          <span>
-            {oauth.email}
-            <Link onClick={onClickLogout}>logout</Link>
-          </span>
-        :
-          <span>new user</span>
-      }
-      
+      {oauth ? (
+        <Grid container direction="row" xs={12}>
+          <Grid item xs alignItems="center" />
+          <Grid item> {oauth.email}</Grid>
+
+          <Grid container xs>
+            <Button
+              variant="contained"
+              color="secondary"
+              endIcon={<LogoutIcon />}
+              onClick={onClickLogout}
+            >
+              Logout
+            </Button>
+          </Grid>
+        </Grid>
+      ) : (
+        <span></span>
+      )}
     </header>
   );
 }
--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -5,6 +5,12 @@ import crypto from "crypto";
 import qs from "qs";
 import axios from "axios";
 // Layout
+import React from "react";
+import PropTypes from "prop-types";
+import ButtonUnstyled, {
+  buttonUnstyledClasses,
+} from "@mui/core/ButtonUnstyled";
+import { styled } from "@mui/system";
 import { useTheme } from "@mui/styles";
 import { Link } from "@mui/material";
 // Local
@@ -45,6 +51,112 @@ const useStyles = (theme) => ({
   },
 });
 
+//Button MUI
+const ButtonRoot = React.forwardRef(function ButtonRoot(props, ref) {
+  const { children, ...other } = props;
+
+  return (
+    <svg width="150" height="50" {...other} ref={ref}>
+      <polygon points="0,50 0,0 150,0 150,50" className="bg" />
+      <polygon points="0,50 0,0 150,0 150,50" className="borderEffect" />
+      <foreignObject x="0" y="0" width="150" height="50">
+        <div className="content">{children}</div>
+      </foreignObject>
+    </svg>
+  );
+});
+
+ButtonRoot.propTypes = {
+  children: PropTypes.node,
+};
+
+const CustomButtonRoot = styled(ButtonRoot)(
+  ({ theme }) => `
+  overflow: visible;
+  cursor: pointer;
+  --main-color: ${
+    theme.palette.mode === "light" ? "rgb(25,118,210)" : "rgb(144,202,249)"
+  };
+  --hover-color: ${
+    theme.palette.mode === "light"
+      ? "rgba(25,118,210,0.04)"
+      : "rgba(144,202,249,0.08)"
+  };
+  --active-color: ${
+    theme.palette.mode === "light"
+      ? "rgba(25,118,210,0.12)"
+      : "rgba(144,202,249,0.24)"
+  };
+
+  & polygon {
+    fill: transparent;
+    transition: all 800ms ease;
+    pointer-events: none;
+  }
+  
+  & .bg {
+    stroke: var(--main-color);
+    stroke-width: 0.5;
+    filter: drop-shadow(0 4px 20px rgba(0, 0, 0, 0.1));
+    fill: transparent;
+  }
+
+  & .borderEffect {
+    stroke: var(--main-color);
+    stroke-width: 2;
+    stroke-dasharray: 150 600;
+    stroke-dashoffset: 150;
+    fill: transparent;
+  }
+
+  &:hover,
+  &.${buttonUnstyledClasses.focusVisible} {
+    .borderEffect {
+      stroke-dashoffset: -600;
+    }
+
+    .bg {
+      fill: var(--hover-color);
+    }
+  }
+
+  &:focus,
+  &.${buttonUnstyledClasses.focusVisible} {
+    outline: none;
+  }
+
+  &.${buttonUnstyledClasses.active} { 
+    & .bg {
+      fill: var(--active-color);
+      transition: fill 300ms ease-out;
+    }
+  }
+
+  & foreignObject {
+    pointer-events: none;
+
+    & .content {
+      font-family: Helvetica, Inter, Arial, sans-serif;
+      font-size: 14px;
+      font-weight: 200;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: var(--main-color);
+      text-transform: uppercase;
+    }
+
+    & svg {
+      margin: 0 5px;
+    }
+  }`
+);
+
+const SvgButton = React.forwardRef(function SvgButton(props, ref) {
+  return <ButtonUnstyled {...props} component={CustomButtonRoot} ref={ref} />;
+});
+
 const Redirect = ({ config, codeVerifier }) => {
   const styles = useStyles(useTheme());
   const redirect = (e) => {
@@ -63,9 +175,9 @@ const Redirect = ({ config, codeVerifier }) => {
   };
   return (
     <div css={styles.root}>
-      <Link onClick={redirect} color="secondary">
-        Login with OpenID Connect and OAuth2
-      </Link>
+      <SvgButton onClick={redirect} color="secondary">
+        Login
+      </SvgButton>
     </div>
   );
 };
@@ -120,12 +232,11 @@ const LoadToken = ({ code, codeVerifier, config, removeCookie, setOauth }) => {
 
 export default function Login({ onUser }) {
   const styles = useStyles(useTheme());
-  // const location = useLocation();
   const [cookies, setCookie, removeCookie] = useCookies([]);
   const { oauth, setOauth } = useContext(Context);
   const config = {
-    authorization_endpoint: "http://localhost:5556/dex/auth",
-    token_endpoint: "http://localhost:5556/dex/token",
+    authorization_endpoint: "http://127.0.0.1:5556/dex/auth",
+    token_endpoint: "http://127.0.0.1:5556/dex/token",
     client_id: "webtech-frontend",
     redirect_uri: "http://127.0.0.1:3000",
     scope: "openid%20email%20offline_access",
